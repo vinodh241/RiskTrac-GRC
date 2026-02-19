cd429ffa75ab1da95c455bc32a05ff387fbcdd05
vinodh241 <a.vinodh9072@gmail.com>
2026-02-19 18:20:24 +0530

    SE-RiskTrac

--- bcmweb/src/environments/environment.prod.ts
+++ bcmweb/src/environments/environment.prod.ts
@@ -5,5 +5,13 @@ export const environment = {
   ormapiUrl: '/ormapi',
   hostUrl: "http://10.0.1.32:8080",
   bcmapiUrl: '/bcmapi',  // root-relative: requests go to /bcmapi/ not /bcm/bcmapi/
-  currency: 'SAR'
-  };
+  devConfig: false,
+  client: "AMLAK",
+  ckeditorUrl: 'http://10.0.1.32:8080/assets/js/ckeditor/ckeditor.js',
+  currency: 'SAR',
+  defaultLanguage: 'en',
+  MasterData: {
+    PageName: ['Impact'],
+    TotalRecords: 6
+  }
+};
