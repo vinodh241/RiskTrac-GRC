e19d72884e236b09ef271b54320674960060a296
vinodh241 <a.vinodh9072@gmail.com>
2026-02-19 18:15:45 +0530

    SE-RiskTrac

--- bcmweb/src/environments/environment.prod.ts
+++ bcmweb/src/environments/environment.prod.ts
@@ -1,16 +1,9 @@
 export const environment = {
   dummyData: false,
   production: true,
-  umapiUrl:'umapi',
-  ormapiUrl:'ormapi',
-  hostUrl: "http://risktracdev.secureyes.net:5000",
-  bcmapiUrl:'bcmapi',
-  devConfig: false,
-  ckeditorUrl: 'http://risktracdev.secureyes.net:5000/bcm/assets/js/ckeditor/ckeditor.js',
-  currency: 'INR',
-  defaultLanguage: 'en',
-  MasterData : {
-    PageName :['Impact'],
-    TotalRecords : 2
-  }
-};
+  umapiUrl: '/umapi',
+  ormapiUrl: '/ormapi',
+  hostUrl: "http://10.0.1.32:8080",
+  bcmapiUrl: '/bcmapi',  // root-relative: requests go to /bcmapi/ not /bcm/bcmapi/
+  currency: 'SAR'
+  };
