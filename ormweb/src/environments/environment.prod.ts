e9ff862e90a3558ff8904f17fde765ace89f9742
vinodh241 <a.vinodh9072@gmail.com>
2026-02-20 14:42:59 +0530

    SE-RiskTrac

--- ormweb/src/environments/environment.prod.ts
+++ ormweb/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   dummyData: false,
   production: true,
-  umapiUrl: 'umapi',
+  umapiUrl: '/umapi',  // root-relative so logout/auth go to /umapi/... (not /orm/umapi/...)
   ormapiUrl: '/ormapi',
   hostUrl: "http://10.0.1.32:8080",
   bcmapiUrl: 'bcmapi',
